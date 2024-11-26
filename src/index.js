397f1782149cfdd310a6ddee6e39403ec949032d
hemanbhullar <hemanbhullar8082@gmail.com>
2024-11-26 08:54:15 +0530

    Added basic routing layer

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { StatusCodes } from 'http-status-codes';
 
 import ConnectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
+import apiRouter from './routers/apiRouter.js';
 
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({
---
+++ src/routers/v1/user.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: "Successfully"
+    })
+})
+
+export default router;
