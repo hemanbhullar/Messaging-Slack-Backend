a22c44b7ca67e7fe016fd0df19ee65fb7fead4ce
hemanbhullar <hemanbhullar8082@gmail.com>
2024-11-27 07:35:45 +0530

    Added repository signature

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+export const getUserByEmail = (email) => {=
+};
+
+export const getUserByName = (name) => {
+
+}
+
+export const createUser = (user) => {
+
+};
+
+export const getUsers = () => {
+
+};
+
+
+export const getUserById = (id) => {};
+
+export const deleteUser = (id) => {
+
+};
+
+export const updateUser = () => {
+
+};
