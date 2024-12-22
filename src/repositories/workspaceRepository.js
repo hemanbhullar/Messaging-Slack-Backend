86a1b7fed716113abb5a528f59b13922c1297cd9
hemanbhullar <hemanbhullar8082@gmail.com>
2024-12-22 18:20:15 +0530

    Added workspace repo functionality

--- src/repositories/workspaceRepository.js
+++ src/repositories/workspaceRepository.js
@@ -1,23 +1,116 @@
-import { get } from "mongoose";
+import { StatusCodes } from "http-status-codes";
 import Workspace from "../schema/workspace.js";
+import ClientError from "../utils/errors/clientError.js"
 import crudRepository from "./crudRepository.js";
 
 const workspaceRepository = {
     ...crudRepository(Workspace),//destructoring crudReposity
 
-    getWorkspaveByName: async function () {
+    getWorkspaceByName: async function (workspaceName) {
+        try {
+            const workspace = await Workspace.findOne({ name: workspaceName });
+
+            if(!workspace)  {
+                throw new ClientError({
+                    explaination: 'Invalid data sent from the client',
+                    message: 'Workspace not found',
+                    statusCode: StatusCodes.NOT_FOUND
+                })
+            }
+            return workspace;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    },
+    getWorkspaceByJoinCode: async function (joinCode) {
+        try {
+            const workspace = await Workspace.findOne({ joinCode });
+            if(!workspace)  {
+                throw new ClientError({
+                    explaination: 'Invalid data sent from the client',
+                    message: 'Workspace not found',
+                    statusCode: StatusCodes.NOT_FOUND
+                })
+            }
+            return workspace;
+        } catch (error) {
+            console.log(error); //log error
+            throw error; //throw error
+        }
+    },
+    addMemberToWorkspace: async function (memberId, workspaceId, role) {
+        try {
+            const workspace = await Workspace.findById(workspaceId);
+            if(!workspace)  {
+                throw new ClientError({
+                    explaination: 'Invalid data sent from the client',
+                    message: 'Workspace not found',
+                    statusCode: StatusCodes.NOT_FOUND
+                })
+            }
+            
+            const isValidUser = await User.findById(memberId);
+            if(!isValidUser) {
+                throw new ClientError({
+                    explaination: 'Invalid data sent from the client',
+                    message: 'User not found',
+                    statusCode: StatusCodes.NOT_FOUND
+                })
+            }
+            const member = {
+                memberId,
+                role
+            }
+            if(workspace.members.find(m => m.memberId.toString() === memberId.toString())){
+                throw new ClientError({
+                    explaination: 'Invalid data sent from the client',
+                    message: 'User already part of workspace',
+                    statusCode: StatusCodes.FORBIDDEN
+                })
+            }
+            workspace.members.push(member); //push member to workspace
+            await workspace.save(); //save workspace
+            return workspace;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
 
     },
-    getWorkspaceByJoinCode: async function () {
-
+    addChannelToWorkspace: async function (channelId, workspaceId) {
+        try {
+            const workspace = await Workspace.findById(workspaceId);
+            if(!workspace)  {
+                throw new ClientError({
+                    explaination: 'Invalid data sent from the client',
+                    message: 'Workspace not found',
+                    statusCode: StatusCodes.NOT_FOUND
+                })
+            }
+            workspace.channels.push(channelId);
+            await workspace.save(); //save workspace
+            return workspace;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
     },
-    addMemberToWorkspace: async function () {
-
-    },
-    addChannelToWorkspace: async function () {
-
-    },
-    fetchAllWorkspaceByMemberId: async function () {
+    fetchAllWorkspaceByMemberId: async function (memberId) {
+        try {
+            const workspace = await Workspace.find({ 'members.memberId': memberId });
+            if(!workspace)  {
+                throw new ClientError({
+                    explaination: 'Invalid data sent from the client',
+                    message: 'Workspace not found',
+                    statusCode: StatusCodes.NOT_FOUND
+                })
+            }
+            return workspaces;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
     }
 }
 
